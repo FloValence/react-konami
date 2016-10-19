8c70005848ffc97857c18928cab237f8996b40b4
Jeffrey Koch <jeffrey.koch@lausd.net>
2016-10-19 15:51:45 -0700

    Bugfixes
    Updated proptypes and references.

--- Konami.js
+++ Konami.js
@@ -21,7 +21,7 @@ class Konami extends Component {
             return false
         }
     } else {
-        n = 0
+        this.n = 0
     }
   }
 
@@ -32,13 +32,10 @@ class Konami extends Component {
 
 Konami.propTypes = {
   easterEgg: PropTypes.func.isRequired,
-  konami: PropTypes.string
+  konami: PropTypes.arrayOf(PropTypes.number)
 }
 
 Konami.defaultProps = {
-  easterEgg() {
-    alert('Hell Yeah !!')
-  },
   konami: [38,38,40,40,37,39,37,39,66,65]
 }
 
