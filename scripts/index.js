58166810657292bfff621094452bd56ab5e52a03
Florian Valence <fvalence@deezer.com>
2019-08-08 15:29:47 +0200

    Use Typescript version of Konami component

--- scripts/index.js
+++ scripts/index.js
@@ -1,125 +1,98 @@
 'use strict';
 
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Konami = function (_Component) {
-  _inherits(Konami, _Component);
-
-  function Konami(props) {
-    _classCallCheck(this, Konami);
-
-    var _this = _possibleConstructorReturn(this, (Konami.__proto__ || Object.getPrototypeOf(Konami)).call(this, props));
-
-    _this.n = 0;
-    _this.delayId = null;
-    _this.onKeydown = _this.onKeydown.bind(_this);
-    return _this;
-  }
-
-  _createClass(Konami, [{
-    key: 'componentDidMount',
-    value: function componentDidMount() {
-      document.addEventListener('keydown', this.onKeydown);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Konami = /** @class */ (function (_super) {
+    __extends(Konami, _super);
+    function Konami() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.n = 0;
+        _this.delayId = 0;
+        _this.onKeydown = function (e) {
+            var _a = _this.props, _b = _a.konami, konami = _b === void 0 ? [] : _b, easterEgg = _a.easterEgg;
+            if (e.keyCode === konami[_this.n]) {
+                _this.n++;
+                _this.delayOn();
+                if (_this.n === konami.length) {
+                    easterEgg();
+                    _this.resetN();
+                    _this.delayOff();
+                    return;
+                }
+            }
+            else {
+                _this.resetN(e.keyCode);
+                _this.delayOff();
+            }
+        };
+        return _this;
     }
-  }, {
-    key: 'componentWillReceiveProps',
-    value: function componentWillReceiveProps(nextProps) {
-      var resetDelay = nextProps.resetDelay;
-
-      var resetDelayHasChanged = resetDelay !== this.props.resetDelay;
-      if (resetDelayHasChanged && resetDelay <= 0) {
-        this.delayOff();
-      } else if (resetDelayHasChanged) {
-        this.delayOff();
-        this.delayOn();
-      }
-      if (nextProps.konami.join('') !== this.props.konami.join('')) {
-        this.resetN();
-        this.delayOff();
-      }
-    }
-  }, {
-    key: 'componentWillUnmount',
-    value: function componentWillUnmount() {
-      document.removeEventListener('keydown', this.onKeydown);
-      this.delayOff();
-    }
-  }, {
-    key: 'delayOff',
-    value: function delayOff() {
-      if (this.delayId) clearTimeout(this.delayId);
-    }
-  }, {
-    key: 'delayOn',
-    value: function delayOn() {
-      var _this2 = this;
-
-      if (this.props.resetDelay <= 0) return;
-      this.delayOff();
-      this.delayId = setTimeout(function () {
-        return _this2.resetN();
-      }, this.props.resetDelay);
-    }
-  }, {
-    key: 'resetN',
-    value: function resetN(keyCode) {
-      if (!keyCode) {
-        this.n = 0;
-        return;
-      }
-
-      var count = 1;
-      while (this.n-- > 0 && this.props.konami[this.n] === keyCode) {
-        count++;
-      }this.n = 0;
-      while (count-- > 0 && this.props.konami[this.n] === keyCode) {
-        this.n++;
-      }
-    }
-  }, {
-    key: 'onKeydown',
-    value: function onKeydown(e) {
-      if (e.keyCode === this.props.konami[this.n]) {
-        this.n++;
-        this.delayOn();
-        if (this.n === this.props.konami.length) {
-          this.props.easterEgg();
-          this.resetN();
-          this.delayOff();
-          return false;
+    Konami.prototype.componentDidMount = function () {
+        document.addEventListener('keydown', this.onKeydown);
+    };
+    Konami.prototype.componentDidUpdate = function (prevProps) {
+        var _a = prevProps.resetDelay, resetDelay = _a === void 0 ? 0 : _a, _b = prevProps.konami, konami = _b === void 0 ? [] : _b;
+        var resetDelayHasChanged = resetDelay !== this.props.resetDelay;
+        if (resetDelayHasChanged && resetDelay <= 0) {
+            this.delayOff();
         }
-      } else {
-        this.resetN(e.keyCode);
+        else if (resetDelayHasChanged) {
+            this.delayOff();
+            this.delayOn();
+        }
+        if (konami.join('') !== (this.props.konami || []).join('')) {
+            this.resetN();
+            this.delayOff();
+        }
+    };
+    Konami.prototype.componentWillUnmount = function () {
+        document.removeEventListener('keydown', this.onKeydown);
         this.delayOff();
-      }
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      return null;
-    }
-  }]);
-
-  return Konami;
-}(React.Component);
-
-Konami.propTypes = {
-  easterEgg: React.PropTypes.func.isRequired,
-  konami: React.PropTypes.arrayOf(React.PropTypes.number),
-  resetDelay: React.PropTypes.number
-};
-
-Konami.defaultProps = {
-  resetDelay: 2000,
-  konami: [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-};
+    };
+    Konami.prototype.delayOff = function () {
+        if (this.delayId)
+            clearTimeout(this.delayId);
+    };
+    Konami.prototype.delayOn = function () {
+        var _this = this;
+        if ((this.props.resetDelay || 0) <= 0)
+            return;
+        this.delayOff();
+        this.delayId = setTimeout(function () { return _this.resetN(); }, this.props.resetDelay);
+    };
+    Konami.prototype.resetN = function (keyCode) {
+        if (!keyCode) {
+            this.n = 0;
+            return;
+        }
+        var _a = this.props.konami, konami = _a === void 0 ? [] : _a;
+        var count = 1;
+        while (this.n-- > 0 && konami[this.n] === keyCode)
+            count++;
+        this.n = 0;
+        while (count-- > 0 && konami[this.n] === keyCode)
+            this.n++;
+    };
+    Konami.prototype.render = function () {
+        return null;
+    };
+    Konami.defaultProps = {
+        resetDelay: 2000,
+        konami: [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+    };
+    return Konami;
+}(React.Component));
 
 var HelloWorld = React.createClass({
   getInitialState: function getInitialState() {
